1017aeb15aa1654a82def61bf67f8529afedfa80
Mike <tauremike@gmail.com>
2023-12-02 16:36:26 -0600

    working on dat 1

--- Day1/main.c
+++ Day1/main.c
@@ -1,17 +1,47 @@
+#include "ctype.h"
 #include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
 int main() {
   FILE *file;
-  char buf[100];
+  char buf[255];
+  int total = 0;
 
-  file = fopen("input.txt", "r");
+  file = fopen("test.txt", "r");
 
   while (fscanf(file, "%s\n", buf) == 1) {
-    for (int i = 0; i < sizeof(buf); i++) {
-      if (isdigit(buf[i])) {
+    int s = strlen(buf);
+    char nums[2] = "\0";
+
+    for (int i = 0; i < s; i++) {
+      char derp = buf[i];
+      if (isdigit(derp)) {
+        printf("%s\n", &derp);
+        nums[0] = derp;
+        break;
       }
     }
 
+    for (int i = s - 1; i >= 0; i--) {
+      char derp = buf[i];
+      if (isdigit(derp)) {
+        printf("%s\n", &derp);
+        nums[1] = derp;
+        break;
+      }
+    }
+
+    printf("%s\n", nums);
+    int num = atoi(nums);
+    printf("%d\n", num);
+    total += num;
+
     return 0;
   }
+
+  fclose(file);
+  printf("Sum: %d\n", total);
+  return 0;
+}
 
